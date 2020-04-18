720bce55e73e5dae5ab11fdd3a74204ddc6d895b
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2020-04-18 13:21:30 +0000

    added choices

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bilesanmiahmadolurunfemi/WebApplications/djangoapps/django-tutorial/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
